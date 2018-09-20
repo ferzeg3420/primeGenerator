f29ccb4d4803e60242049246b7ef91f793795673
Fernando Zegada <ferzeg3420@gmail.com>
2018-09-19 19:04:06 -0700

    Update primeGen.cpp
    Deleted the header and main.cpp to relocate them to their respective files.

--- primeGen.cpp
+++ primeGen.cpp
@@ -2,28 +2,6 @@
 #include <iostream>
 using namespace std;
 
-class PrimeList 
-{
-public:
-   static const int DEFAULT_LIST_LEN = 1000;
-
-private:
-   long listLength;
-   int list[];
-
-public:
-   PrimeList(long listLen);
-   void generatePrimeList();
-   friend ostream& operator<<(ostream &strm, const PrimeList& pl);
-};
-
-
-
-
-int main(int argc, const char * argv[]) 
-{
-}
-
 PrimeList::PrimeList(long listlen)
 {
    this->listLength = listlen;
@@ -50,7 +28,6 @@ void PrimeList::generatePrimeList()
    }
 
 }
-
 
 ostream& operator<<(ostream &strm, const PrimeList& pl) 
 {
