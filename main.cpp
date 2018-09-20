4c3bda247da9c5cc04db3da701f71186a1bc5ba2
Fernando Zegada <ferzeg3420@gmail.com>
2018-09-19 19:16:03 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,41 @@
 
+#include <iostream>
+#include "primeGen.h"
+using namespace std;
+
+int main() 
+{
+  return 0;
+}
+
+PrimeList::PrimeList(long listlen)
+{
+   this->listLength = listlen;
+
+   for (int k = 0; k < listlen; k++)
+   {
+      list[k] = 0;
+   }
+}
+
+void PrimeList::generatePrimeList()
+{
+   long listpos;
+   for (int k = 1; k < 100; k += 2) 
+   {
+      for (int i = 3; i < k; k += 2 ) 
+      {
+         if (k % i == 0 || k % 2 == 0) 
+         {
+            break;
+         }
+         list[listpos] = k;
+      }
+   }
+
+}
+
+ostream& operator<<(ostream &strm, const PrimeList& pl) 
+{
+   return strm;
+}
